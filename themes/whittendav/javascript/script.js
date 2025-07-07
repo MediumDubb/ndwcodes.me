37dc78dc82e344a1725050743a756b00579aec49
Noah Whittington <noah.whittington@innismaggiore.com>
2025-07-07 09:32:26 -0400

    nav and js toches

--- themes/whittendav/javascript/script.js
+++ themes/whittendav/javascript/script.js
@@ -4,7 +4,28 @@ jQuery.noConflict();
         AOS.init({
             once: true,
         });
+
         homePgFull();
+
+        const debounce = (fn) => {
+            // This holds the requestAnimationFrame reference, so we can cancel it if we wish
+            let frame;
+            // The debounce function returns a new function that can receive a variable number of arguments
+            return (...params) => {
+                // If the frame variable has been defined, clear it now, and queue for next frame
+                if (frame) {
+                    cancelAnimationFrame(frame);
+                }
+                // Queue our function call for the next frame
+                frame = requestAnimationFrame(() => {
+                    // Call our function and pass any params we received
+                    fn(...params);
+                });
+            }
+        };
+        // Listen for new scroll events, here we debounce our `storeScroll` function
+        window.addEventListener('resize', debounce(homePgFull), { passive: true });
+
         function homePgFull() {
             let documentHeight = $(window).height();
             let footerHeight = $('footer.footer').height();
